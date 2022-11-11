a4027f1908d9d26471b7d4d00f5e63c6c4c5ff97
Juan <dev@juanaleman.com>
2022-11-11 13:12:32 -0400

    multiple block route updates

--- src/routes/block.jsx
+++ src/routes/block.jsx
@@ -3,34 +3,54 @@ import { withRouter } from './shared/classhooks';
 import { getCntrprty } from '../api';
 import { OneElements, ListElements } from './shared/elements';
 // import { ListElements, OnlyElements } from './shared/elements';
+import { Link } from "react-router-dom";
 
 class Block extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            block: props.router.params.block,
+            block: Number(props.router.params.block),
             block_not_found: null,
+            block_row: null,
             messages: null,
         };
     }
 
     async fetchData(block) {
-        const block_response = await getCntrprty(`/block/${block}`);
+        let block_response = {};
+        try {
+            block_response = await getCntrprty(`/block/${block}`);
+        }
+        catch (e) {
+            block_response.messages = null;
+        }
+        // const block_response = await getCntrprty(`/block/${block}`);
 
         // console.log(`rrr1`);
         // console.log(JSON.stringify(block_response));
         // console.log(`rrr2`);
 
         if (!block_response.messages) {
-            this.setState({ block_not_found: true });
+            this.setState({
+                block,
+                block_not_found: true,
+                block_row: null,
+                messages: null,
+            });
         }
         else if (!block_response.messages.length) {
             this.setState({
+                block,
+                block_not_found: null,
+                block_row: block_response.block_row,
                 messages: [],
             });
         }
         else { // block_response.messages.length
             this.setState({
+                block,
+                block_not_found: null,
+                block_row: block_response.block_row,
                 messages: block_response.messages,
             });
         }
@@ -40,6 +60,27 @@ class Block extends React.Component {
     async componentDidMount() {
         // not awaiting it
         this.fetchData(this.state.block);
+    }
+
+    async componentDidUpdate(prevProps) {
+
+        // console.log(`rrrrrrrrr1`);
+        // console.log(JSON.stringify(this.props));
+        // console.log(this.props.router.params.block);
+        // console.log(`rrrrrrrrr2`);
+        // console.log(JSON.stringify(prevProps));
+        // console.log(prevProps.router.params.block);
+        // console.log(`rrrrrrrrr3`);
+
+        // props.router.params.block
+        const updatedProp = Number(this.props.router.params.block);
+        // const updatedProp = this.props.router.params.block;
+        // const updatedProp = this.props.block;
+        if (updatedProp !== Number(prevProps.router.params.block)) {
+            // if (updatedProp !== prevProps.router.params.block) {
+            // if (updatedProp !== prevProps.block) {
+            await this.fetchData(updatedProp);
+        }
     }
 
     render() {
@@ -77,10 +118,63 @@ class Block extends React.Component {
                 </>
             );
         }
+
+        let change_pages_element = (<p>loading...</p>);
+        if (this.state.block) {
+            /////
+            const previous_page_column = (
+                <td>
+                    <Link to={`/block/${this.state.block - 1}`}>{'<'}previous</Link>{' '}
+                </td>
+            );
+            const next_page_column = (
+                <td>
+                    <Link to={`/block/${this.state.block + 1}`}>next{'>'}</Link>{' '}
+                </td>
+            );
+
+            change_pages_element = (
+                <table>
+                    <tbody>
+                        <tr style={{ padding: "0.25rem" }}>
+                            {previous_page_column}
+                            {next_page_column}
+                        </tr>
+                    </tbody>
+                </table>
+            );
+            /////
+        }
+
+        let block_metadata = null;
+        // let block_metadata = (<p>loading...</p>);
+        if (this.state.block_row) {
+            block_metadata = (
+                <>
+                    <ul>
+                        <li>block_index: {this.state.block}</li>
+                        {/* <li>block_time: {this.state.block_row.block_time}</li> */}
+                        <li>block_time_iso: {(new Date(this.state.block_row.block_time * 1000).toISOString()).replace('.000Z', 'Z')}</li>
+                    </ul>
+                    <ul>
+                        <li>block_hash: {this.state.block_row.block_hash}</li>
+                        <li>previous_block_hash: {this.state.block_row.previous_block_hash}</li>
+                    </ul>
+                    <ul>
+                        <li>ledger_hash: {this.state.block_row.ledger_hash}</li>
+                        <li>txlist_hash: {this.state.block_row.txlist_hash}</li>
+                        <li>messages_hash: {this.state.block_row.messages_hash}</li>
+                    </ul>
+                </>
+            );
+        }
+
         const block_element = (
             <>
                 <h2>Bitcoin block: {this.state.block}</h2>
                 {/* <h2>Block: {this.state.block}</h2> */}
+                {change_pages_element}
+                {block_metadata}
                 {block_element_contents}
             </>
         );
