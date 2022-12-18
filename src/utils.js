5b123ffe5dd3b982dd4f0d83dbde22f6caec1f75
Juan <dev@juanaleman.com>
2022-12-17 22:14:46 -0400

    adding timeSince util

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,31 @@ function timeIsoFormat(block_time) {
     return (new Date(block_time * 1000).toISOString()).replace('.000Z', 'Z');
 }
 
+// https://stackoverflow.com/a/47006398
+///////////////////////////////////////
+const intervals = [
+    { label: 'year', seconds: 31536000 },
+    { label: 'month', seconds: 2592000 },
+    { label: 'day', seconds: 86400 },
+    { label: 'hour', seconds: 3600 },
+    { label: 'minute', seconds: 60 },
+    { label: 'second', seconds: 1 }
+];
+
+function timeSince(date) {
+    const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+    const interval = intervals.find(i => i.seconds < seconds);
+    const count = Math.floor(seconds / interval.seconds);
+    return `${count} ${interval.label}${count !== 1 ? 's' : ''} ago`;
+}
+///////////////////////////////////////
+
 function quantityWithDivisibility(divisible, quantity_integer) {
     return divisible ? (quantity_integer / (10 ** 8)).toFixed(8) : quantity_integer;
 }
 
 export {
     timeIsoFormat,
+    timeSince,
     quantityWithDivisibility,
 };
