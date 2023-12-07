5afa49212a2092cbc114b9db525f5c1e623af943
Juan <dev@juanaleman.com>
2023-12-07 14:35:18 -0600

    version bump

--- src/routes/shared/elements.js
+++ src/routes/shared/elements.js
@@ -1130,7 +1130,7 @@ class OneElements {
             <main style={{ padding: "1rem" }}>
                 {route_element}
                 <p>
-                    [<a href={`https://github.com/CNTRPRTY/xcpdev`} target="_blank">xcp.dev v1.0</a>]
+                    [<a href={`https://github.com/CNTRPRTY/xcpdev`} target="_blank">xcp.dev v1.1</a>]
                     <br />
                     [counterparty-lib v{COUNTERPARTY_VERSION}]
                     <br />
