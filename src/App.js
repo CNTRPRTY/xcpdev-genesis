3c435b24f9c795df38d3fde218d5566c3875a75c
Juan <dev@juanaleman.com>
2024-01-28 08:49:55 -0600

    simple app module, just the css and router outlet

--- src/App.js
+++ src/App.js
@@ -1,64 +1,16 @@
-import React from 'react';
-import { Outlet, Link } from "react-router-dom";
-// import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { Outlet } from "react-router-dom";
+
 class App extends React.Component {
-  // function App() {
   render() {
     return (
-
       <main className="App">
-
-        <div style={{ padding: "1rem" }}>
-
-          <h1>xcp.dev</h1>
-          <h3>Counterparty Bitcoin Tools</h3>
-          {/* <h3>Counterparty Bitcoin data explorer</h3> */}
-          {/* <h3>Counterparty Bitcoin explorer</h3> */}
-          {/* <h3>Counterparty Bitcoin block explorer</h3> */}
-
-          <nav
-            style={{
-              borderBottom: "solid 1px",
-              paddingBottom: "1rem",
-            }}
-          >
-
-            <Link to="/">Data</Link> |{" "}
-            <Link to="/wallet">Wallet</Link>
-            {/* <Link to="/">Home</Link> */}
-            {/* <Link to="/">Mempool</Link> |{" "}
-            <Link to="/blocks">Blocks</Link> */}
-
-          </nav>
-          <Outlet />
-        </div>
-
+        <Outlet />
       </main>
-
     );
-
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <p>
-    //         Edit <code>src/App.js</code> and save to reload.
-    //       </p>
-    //       <a
-    //         className="App-link"
-    //         href="https://reactjs.org"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Learn React
-    //       </a>
-    //     </header>
-    //   </div>
-    // );
   }
-
 }
 
 export default App;
