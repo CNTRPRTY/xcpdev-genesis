f6a35301836869911933ec406188e7b9a180e82f
Juan <dev@juanaleman.com>
2024-01-30 08:02:24 -0600

    block page refactor for split messages and surfacing errors

--- src/routes/block.jsx
+++ src/routes/block.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { withRouter } from './shared/classhooks';
 import { getCntrprty } from '../api';
 import { OneElements, ListElements } from './shared/elements';
-// import { ListElements, OnlyElements } from './shared/elements';
 import { Link } from "react-router-dom";
 
 function baseState(block) {
     return {
         block,
-        // block: Number(props.router.params.block),
-        block_not_found: null,
+
+        // TODO make it non-cntrprty compatible
+
+        // block_not_found: false,
+        block_row_loading: true,
+        block_row_loading_error: null,
         block_row: null,
-        messages: null,
+
+        messages_loading: true,
+        messages_loading_error: null,
+        messages: [],
     };
 }
 
@@ -23,8 +29,6 @@ class Block extends React.Component {
 
     async fetchData(_block) {
 
-        let block_response = {};
-
         // handle blockhash redirect
         if (!Number.isInteger(Number(_block))) {
             // assume is blockhash
@@ -32,9 +36,9 @@ class Block extends React.Component {
                 const blockindex_response = await getCntrprty(`/blockhash/${_block}`);
                 this.props.router.navigate(`/block/${blockindex_response.block_row.block_index}`, { replace: true });
             }
-            catch (e) {
+            catch (err) {
                 this.setState({
-                    block_not_found: true,
+                    block_row_loading_error: err,
                 });
             }
         }
@@ -44,41 +48,44 @@ class Block extends React.Component {
 
             this.setState(baseState(block));
 
+            let block_row = null;
             try {
-                block_response = await getCntrprty(`/block/${block}`);
+                const block_response = await getCntrprty(`/block/${block}`);
+                block_row = block_response.block_row;
+                this.setState({
+                    block_row_loading: false,
+                    block_row,
+                });
             }
-            catch (e) {
-                block_response.messages = null;
-            }
-            // const block_response = await getCntrprty(`/block/${block}`);
+            catch (err) {
 
-            // console.log(`rrr1`);
-            // console.log(JSON.stringify(block_response));
-            // console.log(`rrr2`);
+                // TODO: not found would get here, right?
 
-            if (!block_response.messages) {
                 this.setState({
-                    block,
-                    block_not_found: true,
-                    block_row: null,
-                    messages: null,
+                    block_row_loading_error: err,
+                    // block_not_found: true,
                 });
             }
-            else if (!block_response.messages.length) {
+
+            if (!block_row) {
                 this.setState({
-                    block,
-                    block_not_found: null,
-                    block_row: block_response.block_row,
-                    messages: [],
+                    messages_loading: false,
+                    messages_loading_error: Error(`no block`),
                 });
             }
-            else { // block_response.messages.length
-                this.setState({
-                    block,
-                    block_not_found: null,
-                    block_row: block_response.block_row,
-                    messages: block_response.messages,
-                });
+            else { // block_row
+                try {
+                    const messages_response = await getCntrprty(`/block/${block}/messages`);
+                    this.setState({
+                        messages_loading: false,
+                        messages: messages_response.messages,
+                    });
+                }
+                catch (err) {
+                    this.setState({
+                        messages_loading_error: err,
+                    });
+                }
             }
 
         }
@@ -86,104 +93,27 @@ class Block extends React.Component {
     }
 
     async componentDidMount() {
-        // not awaiting it
-        this.fetchData(this.state.block);
+        await this.fetchData(this.state.block);
     }
 
     async componentDidUpdate(prevProps) {
-
-        // console.log(`rrrrrrrrr1`);
-        // console.log(JSON.stringify(this.props));
-        // console.log(this.props.router.params.block);
-        // console.log(`rrrrrrrrr2`);
-        // console.log(JSON.stringify(prevProps));
-        // console.log(prevProps.router.params.block);
-        // console.log(`rrrrrrrrr3`);
-
-        // props.router.params.block
-        // const updatedProp = Number(this.props.router.params.block);
         const updatedProp = this.props.router.params.block;
-        // const updatedProp = this.props.block;
         if (`${updatedProp}`.trim() !== `${prevProps.router.params.block}`.trim()) {
-            // if (updatedProp !== Number(prevProps.router.params.block)) {
-            // if (updatedProp !== prevProps.router.params.block) {
-            // if (updatedProp !== prevProps.block) {
             await this.fetchData(updatedProp);
         }
     }
 
     render() {
 
-        let block_element_contents = (<p>loading...</p>);
-        if (this.state.block_not_found) {
-            block_element_contents = (
-                <p>block not found</p>
-            );
+        let block_metadata_element = (<p>loading...</p>);
+        if (this.state.block_row_loading_error) {
+            block_metadata_element = (<p>{`${this.state.block_row_loading_error}`}</p>);
         }
-        else if (this.state.messages && !this.state.messages.length) {
-            block_element_contents = (
-                <>
-                    <h3>Messages:</h3>
-                    <p>no messages in block</p>
-                </>
-            );
-        }
-        else if (this.state.messages && this.state.messages.length) {
-            block_element_contents = (
-                <>
-                    <h3>Messages ({this.state.messages.length}):</h3>
-                    {/* <h3>Messages:</h3> */}
-
-                    <table>
-                        <tbody>
-                            {ListElements.getTableRowMessageBlockHeader()}
-                            {this.state.messages.map((message_row, index) => {
-                                return ListElements.getTableRowMessageBlock(message_row, index);
-                                // const page = 'home'; // TODO?
-                                // return ListElements.getTableRowMessage(message_row, index, page);
-                            })}
-                        </tbody>
-                    </table>
-
-                </>
-            );
-        }
-
-        let change_pages_element = (<p>loading...</p>);
-        if (this.state.block) {
-            /////
-            const previous_page_column = (
-                <td>
-                    <Link to={`/block/${this.state.block - 1}`}>{'<'}previous</Link>{' '}
-                </td>
-            );
-            const next_page_column = (
-                <td>
-                    <Link to={`/block/${this.state.block + 1}`}>next{'>'}</Link>{' '}
-                </td>
-            );
-
-            change_pages_element = (
-                <table>
-                    <tbody>
-                        <tr style={{ padding: "0.25rem" }}>
-                            {previous_page_column}
-                            {next_page_column}
-                        </tr>
-                    </tbody>
-                </table>
-            );
-            /////
-        }
-
-        let block_metadata = null;
-        // let block_metadata = (<p>loading...</p>);
-        if (this.state.block_row) {
-            block_metadata = (
+        else if (!this.state.block_row_loading) {
+            block_metadata_element = (
                 <>
                     <ul>
                         <li>block_index: {this.state.block}</li>
-                        {/* <li>block_time: {this.state.block_row.block_time}</li> */}
                         <li>block_time_iso: {(new Date(this.state.block_row.block_time * 1000).toISOString()).replace('.000Z', 'Z')}</li>
                     </ul>
                     <ul>
@@ -194,35 +124,77 @@ class Block extends React.Component {
                         <li>ledger_hash (L): {this.state.block_row.ledger_hash}</li>
                         <li>txlist_hash (TX): {this.state.block_row.txlist_hash}</li>
                         <li>messages_hash (M): {this.state.block_row.messages_hash}</li>
-                        {/* <li>ledger_hash: {this.state.block_row.ledger_hash}</li>
-                        <li>txlist_hash: {this.state.block_row.txlist_hash}</li>
-                        <li>messages_hash: {this.state.block_row.messages_hash}</li> */}
                     </ul>
                 </>
             );
         }
 
+
+        let block_messages_element_header = (<h3>Messages:</h3>);
+
+        let block_messages_element = (<p>loading...</p>);
+        if (this.state.messages_loading_error) {
+            block_messages_element = (<p>{`${this.state.messages_loading_error}`}</p>);
+        }
+        else if (!this.state.messages_loading) {
+
+            if (this.state.messages.length) {
+                block_messages_element_header = (<h3>Messages ({this.state.messages.length}):</h3>);
+            }
+
+            block_messages_element =
+                this.state.messages.length ?
+                    (
+                        <>
+                            <table>
+                                <tbody>
+                                    {ListElements.getTableRowMessageBlockHeader()}
+                                    {this.state.messages.map((message_row, index) => {
+                                        return ListElements.getTableRowMessageBlock(message_row, index);
+                                    })}
+                                </tbody>
+                            </table>
+                        </>
+                    )
+                    : (<p>no messages in block</p>);
+        }
+
+
+        const previous_page_column = (
+            <td>
+                <Link to={`/block/${this.state.block - 1}`}>{'<'}previous</Link>{' '}
+            </td>
+        );
+        const next_page_column = (
+            <td>
+                <Link to={`/block/${this.state.block + 1}`}>next{'>'}</Link>{' '}
+            </td>
+        );
+        const change_pages_element = (
+            <table>
+                <tbody>
+                    <tr style={{ padding: "0.25rem" }}>
+                        {previous_page_column}
+                        {next_page_column}
+                    </tr>
+                </tbody>
+            </table>
+        );
+
+
         const block_element = (
             <>
                 <h2>Bitcoin block: {this.state.block}</h2>
-                {/* <h2>Block: {this.state.block}</h2> */}
+
                 {change_pages_element}
-                {block_metadata}
-                {block_element_contents}
+                {block_metadata_element}
+
+                {block_messages_element_header}
+                {block_messages_element}
             </>
         );
 
         return OneElements.getFullPageForRouteElement(block_element);
-        // return (
-        //     <main style={{ padding: "1rem" }}>
-        //         {block_element}
-        //         <p>
-        //             [xcp.dev v1.0]
-        //             <br />
-        //             [counterparty-lib v9.59] in [Bitcoin Core v0.21]
-        //         </p>
-        //     </main>
-        // );
     }
 
 }
