7fa09f29efbc63bdcbddb3869082323cb5dbc45c
Juan <dev@juanaleman.com>
2024-03-18 13:14:27 -0500

    comments cleanup (no code change)

--- server/express_server/src/index.js
+++ server/express_server/src/index.js
@@ -51,51 +51,14 @@ app.get('/tip', async (req, res) => {
 });
 
 app.get('/mempool', async (req, res) => {
-    // const mempool = await Queries.getMempoolRows(db);
     res.status(200).json({
-        // node: {
-        //     BITCOIN_VERSION,
-        //     COUNTERPARTY_VERSION,
-        // },
         mempool: cached_mempool,
-        // mempool,
     });
 });
 
 app.get('/blocks', async (req, res) => {
-    // TODO redo when the latest block is in memory
-
-    // const blocks = await Queries.getMessagesByBlockLatest(db);
-
-    // const from_block_index = blocks.reduce(function (prev, curr) {
-    //     // minimum
-    //     return prev.block_index < curr.block_index ? prev : curr;
-    // });
-
-    // let blocks_all = await Queries.getBlocksLatest(db, from_block_index.block_index);
-
-    // const block_messages_dict = {};
-    // for (const block of blocks) {
-    //     block_messages_dict[block.block_index] = block.messages;
-    // }
-
-    // blocks_all = blocks_all.map((row) => {
-    //     let messages_count = block_messages_dict[row.block_index] ? block_messages_dict[row.block_index] : 0;
-    //     return {
-    //         ...row,
-    //         messages_count,
-    //     };
-
-    // });
-
     res.status(200).json({
-        // node: {
-        //     BITCOIN_VERSION,
-        //     COUNTERPARTY_VERSION,
-        // },
         blocks: cached_blocks,
-        // blocks: blocks_all,
-        // blocks,
     });
 
 });
@@ -366,20 +329,7 @@ app.get('/subasset/:assetLongname', async (req, res) => {
 
 // only latest
 app.get('/transactions', async (req, res) => {
-    // const btc_transactions_latest = await Queries.getTransactionsLatest(db);
-    // if (!btc_transactions_latest.length) {
-    //     res.status(404).json({
-    //         error: '404 Not Found'
-    //     });
-    // }
-    // else {
-    //     res.status(200).json({
-    //         btc_transactions_latest, // TODO? rename?
-    //     });
-
-    // }
     res.status(200).json({
-        // TODO rename!
         btc_transactions_latest: cached_transactions,
         transactions_latest: cached_transactions,
     });
