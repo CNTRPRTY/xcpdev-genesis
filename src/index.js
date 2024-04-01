098a7979825568835bca4fcf0768fee5f06a2b2a
Juan <dev@juanaleman.com>
2024-04-01 15:46:13 -0400

    missing blocks page route setup

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import Address from './routes/address';
 import Asset from './routes/asset';
 import Transactionspage from './routes/transactions_page';
 import Messagespage from './routes/messages_page';
+import Blockspage from './routes/blocks_page';
 import Apidoc from './routes/apidoc';
 import Wallet from './routes/wallet';
 
@@ -34,6 +35,7 @@ root.render(
           <Route path="asset/:assetName" element={<Asset />} /> {/* for now only the asset_name, not the asset_longname */}
           <Route path="transactions" element={<Transactionspage />} />
           <Route path="messages" element={<Messagespage />} />
+          <Route path="blocks" element={<Blockspage />} />
           <Route path="api" element={<Apidoc />} />
           <Route path="wallet/:address" element={<Wallet />} />
           <Route path="wallet" element={<Wallet />} />
