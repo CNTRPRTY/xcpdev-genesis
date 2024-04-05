788a45947f90ab77ec4132bb3cea27326caa4d61
Juan <dev@juanaleman.com>
2024-04-05 16:17:28 -0400

    format (no code change)

--- src/routes/wallet_create_broadcast.jsx
+++ src/routes/wallet_create_broadcast.jsx
@@ -61,63 +61,63 @@ class WalletCreateBroadcast extends WalletCreate {
                         Params:
                     </p>
                     <div class="py-1 my-1 ml-4">
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td class="pr-1 py-1">
-                                {/* <td> */}
-                                    <span class="dark:text-slate-100">source:</span>
-                                    {/* <span class="text-gray-600 dark:text-gray-400">source:</span> */}
-                                </td>
-                                <td class="py-1">
-                                    <input
-                                        class="border-solid border-2 border-gray-300"
-                                        value={this.state.source}
-                                        size={this.state.source.length}
-                                        disabled={true}
-                                    />
-                                </td>
-                            </tr>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td class="pr-1 py-1">
+                                        {/* <td> */}
+                                        <span class="dark:text-slate-100">source:</span>
+                                        {/* <span class="text-gray-600 dark:text-gray-400">source:</span> */}
+                                    </td>
+                                    <td class="py-1">
+                                        <input
+                                            class="border-solid border-2 border-gray-300"
+                                            value={this.state.source}
+                                            size={this.state.source.length}
+                                            disabled={true}
+                                        />
+                                    </td>
+                                </tr>
 
-                            <tr>
-                                <td class="pr-1 py-1">
-                                    <span class="dark:text-slate-100">text:</span>
-                                </td>
-                                <td class="py-1">
-                                    {/* TODO styling with css file */}
-                                    <textarea
-                                        class="border-solid border-2 border-gray-300"
-                                        rows="2"
-                                        cols="55"
-                                        style={{
-                                            // https://stackoverflow.com/a/658197
-                                            'whiteSpace': "nowrap",
-                                            'overflow': "scroll",
-                                            'overflowY': "hidden",
-                                            'overflowX': "scroll",
-                                            'overflow': "-moz-scrollbars-horizontal",
-                                            // https://stackoverflow.com/a/5271803
-                                            'resize': 'horizontal',
-                                        }} onChange={this.handleTextChange}></textarea>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td class="pr-1 py-1">
+                                        <span class="dark:text-slate-100">text:</span>
+                                    </td>
+                                    <td class="py-1">
+                                        {/* TODO styling with css file */}
+                                        <textarea
+                                            class="border-solid border-2 border-gray-300"
+                                            rows="2"
+                                            cols="55"
+                                            style={{
+                                                // https://stackoverflow.com/a/658197
+                                                'whiteSpace': "nowrap",
+                                                'overflow': "scroll",
+                                                'overflowY': "hidden",
+                                                'overflowX': "scroll",
+                                                'overflow': "-moz-scrollbars-horizontal",
+                                                // https://stackoverflow.com/a/5271803
+                                                'resize': 'horizontal',
+                                            }} onChange={this.handleTextChange}></textarea>
+                                    </td>
+                                </tr>
 
-                            <tr>
-                                <td class="pr-1 py-1">
-                                    <span class="dark:text-slate-100">fee:</span>
-                                </td>
-                                <td class="py-1">
-                                    <input
-                                        class="border-solid border-2 border-gray-300"
-                                        type="text"
-                                        size="8"
-                                        value={this.state.fee}
-                                        onChange={this.handleFeeChange}
-                                    />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                <tr>
+                                    <td class="pr-1 py-1">
+                                        <span class="dark:text-slate-100">fee:</span>
+                                    </td>
+                                    <td class="py-1">
+                                        <input
+                                            class="border-solid border-2 border-gray-300"
+                                            type="text"
+                                            size="8"
+                                            value={this.state.fee}
+                                            onChange={this.handleFeeChange}
+                                        />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     {/* <br /> */}
 
@@ -128,12 +128,12 @@ class WalletCreateBroadcast extends WalletCreate {
                     }
 
                     <div class="py-1 my-1">
-                    <input
-                        class="px-1 border-solid border-2 border-gray-400 dark:text-slate-100"
-                        type="submit"
-                        value="submit"
-                        disabled={this.state.open_dialog_obj !== null}
-                    />
+                        <input
+                            class="px-1 border-solid border-2 border-gray-400 dark:text-slate-100"
+                            type="submit"
+                            value="submit"
+                            disabled={this.state.open_dialog_obj !== null}
+                        />
                     </div>
                 </form>
             </>
