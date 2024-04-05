b639787dc7e568f0423991251eeb24b5b049b681
Juan <dev@juanaleman.com>
2024-04-05 16:16:46 -0400

    wallet create broadcast tailwind pass

--- src/routes/wallet_create_broadcast.jsx
+++ src/routes/wallet_create_broadcast.jsx
@@ -57,48 +57,69 @@ class WalletCreateBroadcast extends WalletCreate {
         return (
             <>
                 <form onSubmit={this.handleSubmit}>
-                    <p>Params:</p>
+                    <p class="text-gray-600 dark:text-gray-400">
+                        Params:
+                    </p>
+                    <div class="py-1 my-1 ml-4">
                     <table>
                         <tbody>
                             <tr>
-                                <td>
-                                    source:
+                                <td class="pr-1 py-1">
+                                {/* <td> */}
+                                    <span class="dark:text-slate-100">source:</span>
+                                    {/* <span class="text-gray-600 dark:text-gray-400">source:</span> */}
                                 </td>
-                                <td>
-                                    <input value={this.state.source} size={this.state.source.length} disabled />
+                                <td class="py-1">
+                                    <input
+                                        class="border-solid border-2 border-gray-300"
+                                        value={this.state.source}
+                                        size={this.state.source.length}
+                                        disabled={true}
+                                    />
                                 </td>
                             </tr>
 
                             <tr>
-                                <td>
-                                    text:
+                                <td class="pr-1 py-1">
+                                    <span class="dark:text-slate-100">text:</span>
                                 </td>
-                                <td>
+                                <td class="py-1">
                                     {/* TODO styling with css file */}
-                                    <textarea rows="2" cols="55" style={{
-                                        // https://stackoverflow.com/a/658197
-                                        'whiteSpace': "nowrap",
-                                        'overflow': "scroll",
-                                        'overflowY': "hidden",
-                                        'overflowX': "scroll",
-                                        'overflow': "-moz-scrollbars-horizontal",
-                                        // https://stackoverflow.com/a/5271803
-                                        'resize': 'horizontal',
-                                    }} onChange={this.handleTextChange}></textarea>
+                                    <textarea
+                                        class="border-solid border-2 border-gray-300"
+                                        rows="2"
+                                        cols="55"
+                                        style={{
+                                            // https://stackoverflow.com/a/658197
+                                            'whiteSpace': "nowrap",
+                                            'overflow': "scroll",
+                                            'overflowY': "hidden",
+                                            'overflowX': "scroll",
+                                            'overflow': "-moz-scrollbars-horizontal",
+                                            // https://stackoverflow.com/a/5271803
+                                            'resize': 'horizontal',
+                                        }} onChange={this.handleTextChange}></textarea>
                                 </td>
                             </tr>
 
                             <tr>
-                                <td>
-                                    fee:
+                                <td class="pr-1 py-1">
+                                    <span class="dark:text-slate-100">fee:</span>
                                 </td>
-                                <td>
-                                    <input type="text" size="8" value={this.state.fee} onChange={this.handleFeeChange} />
+                                <td class="py-1">
+                                    <input
+                                        class="border-solid border-2 border-gray-300"
+                                        type="text"
+                                        size="8"
+                                        value={this.state.fee}
+                                        onChange={this.handleFeeChange}
+                                    />
                                 </td>
                             </tr>
                         </tbody>
                     </table>
-                    <br />
+                    </div>
+                    {/* <br /> */}
 
                     {this.state.open_dialog_obj ?
                         (this.renderDialogObj())
@@ -106,7 +127,14 @@ class WalletCreateBroadcast extends WalletCreate {
                         null
                     }
 
-                    <input type="submit" value="submit" disabled={this.state.open_dialog_obj !== null} />
+                    <div class="py-1 my-1">
+                    <input
+                        class="px-1 border-solid border-2 border-gray-400 dark:text-slate-100"
+                        type="submit"
+                        value="submit"
+                        disabled={this.state.open_dialog_obj !== null}
+                    />
+                    </div>
                 </form>
             </>
 
