51f725c3959ecfca1ae742a1c1d15b1f5912c584
Juan <dev@juanaleman.com>
2024-04-12 18:16:28 -0400

    cleanup

--- src/api.js
+++ src/api.js
@@ -10,7 +10,6 @@ const COUNTERPARTY_VERSION = "9.61.3";
 const API_HOST = "https://api.xcp.dev/v9_61";
 
 
-// https://stackoverflow.com/a/39914235
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -59,46 +58,12 @@ async function getCntrprty(path) {
     // max tries reached
     throw Error(`[202:Accepted] Limit reached, try again.`);
 }
-// async function getCntrprty(path) {
-//     // export async function getCntrprty(path) {
-//     // export async function getCP(path) {
-//     const options = {
-//         method: "GET",
-//     };
-//     let res = {}; // TODO?
-//     const res1 = await fetch(`${api_host}${path}`, options);
-//     // const res = await fetch(`${api_host}${path}`, options);
-//     // const res = await fetch(`${api_host}/mainnet/cp/${path}`, options);
-//     if (!res1.ok) {
-
-//         if (res1.status === 504) { // 504 Gateway Timeout
-
-//             // try one more time
-//             ////////////////////
-//             // sleep a bit
-//             await sleep(1000);
-//             const res2 = await fetch(`${api_host}${path}`, options);
-//             if (!res2.ok) throw Error(`2ndtry[${res2.status}:${res2.statusText}]`);
-//             else res = res2;
-//             ////////////////////
-
-//         }
-
-//         throw Error(`[${res1.status}:${res1.statusText}]`);
-//     }
-//     else {
-//         res = res1;
-//     }
-//     const data = await res.json();
-//     return data.data;
-// }
 
 async function postLibApiProxyFetch(method, params) {
-    // async function postLibApiProxy(method, params) {
 
     const path = "/proxy";
     // const path = "/lib_api_proxy"; // direct to express_server api
-    
+
     const options = {
         method: "POST",
         body: JSON.stringify({
@@ -107,27 +72,8 @@ async function postLibApiProxyFetch(method, params) {
         }),
     };
     return fetch(`${API_HOST}${path}`, options);
-    // const res = await fetch(`${api_host}${path}`, options);
-    // if (!res.ok) {
-    //     if (res.status === 429) {
-    //         const expected_too_many = await res.json();
-    //         throw Error(`[${res.status}:${expected_too_many.message}] Please wait at least 30s before doing another create request from ip:${expected_too_many.ip}.`);
-    //         // throw Error(`[${res.status}:${res.statusText}] ${expected_too_many.ip} 30s`);
-    //     }
-    //     else {
-    //         throw Error(`[${res.status}:${res.statusText}]`);
-    //     }
-    //     // throw Error(`[${res.status}:${res.statusText}]`);
-    // }
-    // const data = await res.json();
-    // return data;
-    // // return data.data;
 }
 
-// async function getBlockMessages(block_height) {
-//     // TODO cache by block_hash to avoid reorg logic
-//     return getCntrprty(`/block/${block_height}`);
-// }
 
 // could cache messages by txid (and block) TODO? "block_messages" instead of messages_all?
 function selectTransactionMessagesFromAll(tx_hash, messages_all) {
@@ -202,17 +148,12 @@ function selectTransactionMessagesFromAll(tx_hash, messages_all) {
 
 export {
     COUNTERPARTY_VERSION_PREVIEW,
-
     COUNTERPARTY_VERSION_ALT,
     COUNTERPARTY_VERSION_ALT_URL,
-
     BITCOIN_VERSION,
     COUNTERPARTY_VERSION,
     API_HOST,
-
     getCntrprty,
     postLibApiProxyFetch,
-    // postLibApiProxy,
-    // getBlockMessages,
     selectTransactionMessagesFromAll,
 };
